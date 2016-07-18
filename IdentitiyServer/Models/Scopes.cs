98b29787f96197dcbd6b7832ec6d540b55c2562c
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-07-18 14:19:44 +0100

    Add project files.

---
+++ IdentitiyServer/Models/Scopes.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentitiyServer.Models {
+    public static class Scopes {
+        public static List<Scope> Get() {
+            return new List<Scope>
+            {
+            StandardScopes.OpenId,
+            StandardScopes.Profile,
+            
+            // additional
+            StandardScopes.Email
+        };
+        }
+    }
+}
